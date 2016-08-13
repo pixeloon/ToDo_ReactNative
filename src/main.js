3942f1f2f70892c61be851c0888097c472ab0fae
Gunther Schneider <pixeloon@gmail.com>
2016-08-13 14:35:33 -0700

    local storage added

--- src/main.js
+++ src/main.js
@@ -3,112 +3,134 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  View,
   TouchableOpacity,
-  View
+  ScrollView,
+  AsyncStorage
 } from 'react-native';
 
 module.exports = React.createClass({
   getInitialState() {
     return ({
-      tasks: ['Learn ReactNative', 'Make Breakfast', 'Drop off Mail'],
+      tasks: [],
       completedTasks: [],
       task: ''
     })
   },
 
+  componentWillMount() {
+    AsyncStorage.getItem('tasks')
+      .then((response) => {
+        this.setState({tasks: JSON.parse(response)})
+      });
+    AsyncStorage.getItem('completedTasks')
+      .then((response) => {
+        this.setState({completedTasks: JSON.parse(response)})
+      });
+  },
+
+  setStorage() {
+    AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    AsyncStorage.setItem('completedTasks', JSON.stringify(this.state.completedTasks));
+  },
+
   renderList(tasks) {
-    // return individual rows  based on the tasks
+    // return individual Views or rows based on the argued tasks
     return (
       tasks.map((task, index) => {
         return (
-          <View key={index} style={styles.task}>
+          <View key={task} style={styles.task}>
             <Text>
               {task}
             </Text>
-            <TouchableOpacity 
-            onPress={() => {this.completeTask(index)}}
+            <TouchableOpacity
+              onPress={()=>this.completeTask(index)}
             >
-                <Text>
-                  &#10003;
-                </Text>
+              <Text>
+                &#10003;
+              </Text>
             </TouchableOpacity>
           </View>
-          )
-      })
-      )
-
-  },
-  renderCompleted(tasks){
-    return (
-    tasks.map((task, index) => {
-      return (
-        <View key={index} style={styles.task}>
-          <Text  style={styles.completed}>
-            {task}
-          </Text>
-            <TouchableOpacity 
-            onPress={() => {this.deleteTask(index)}}
-            >
-                <Text>
-                  &#10005;
-                </Text>
-            </TouchableOpacity>
-        </View>
         )
-      
       })
     )
-
   },
 
-  completeTask(index){
-    console.log('Complete Task:', this.state.tasks[index]);
+  renderCompleted(tasks) {
+    return (
+      tasks.map((task, index) => {
+        return (
+          <View key={task} style={styles.task}>
+            <Text style={styles.completed}>
+              {task}
+            </Text>
+            <TouchableOpacity
+              onPress={()=>this.deleteTask(index)}
+            >
+              <Text>
+                &#10005;
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )
+      })
+    )
+  },
+
+  deleteTask(index) {
+    let completedTasks = this.state.completedTasks;
+    completedTasks = completedTasks.slice(0, index).concat(completedTasks.slice(index+1));
+    this.setState({completedTasks});
+    this.setStorage();
+  },
+
+  completeTask(index) {
     let tasks = this.state.tasks;
-    tasks = tasks.slice(0,index).concat(tasks.slice(index+1));
+    tasks = tasks.slice(0, index).concat(tasks.slice(index+1));
 
     let completedTasks = this.state.completedTasks;
     completedTasks = completedTasks.concat([this.state.tasks[index]]);
+
     this.setState({
-      tasks:tasks,
-      completedTasks: completedTasks
+      tasks,
+      completedTasks
     });
 
-  },
-  addTask() {
-    let tasks = this.state.tasks.concat([this.state.task]);
-    this.setState({tasks:tasks})
-  },
-    deleteTask(index) {
-    let completedTasks = this.state.completedTasks;
-    completedTasks = completedTasks.slice(0, index).concat(completedTasks.slice(index+1))
-    this.setState({completedTasks:completedTasks})
+    this.setStorage();
   },
 
+  addTask() {
+    let tasks = this.state.tasks.concat([this.state.task]);
+    this.setState({tasks})
+    this.setStorage();
+  },
 
   render() {
     return (
-      <View style = {styles.container}>
-        <Text style = {styles.header}>
-          ToDo Master
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          To-Do Master
         </Text>
-        <TextInput 
-        style = {styles.input} 
-        placeholder = 'Add a Task' 
-        onChangeText={(text) => {
-          this.setState({task: text});
-        }} 
-        onEndEditing={() => this.addTask()}
+        <TextInput
+          style={styles.input}
+          placeholder='Add a task...'
+          onChangeText={(text) => {
+            this.setState({task: text});
+          }}
+          onEndEditing={()=>this.addTask()}
         />
-        {this.renderList(this.state.tasks)}
-        {this.renderCompleted(this.state.completedTasks)}
+        <ScrollView>
+          {this.renderList(this.state.tasks)}
+          {this.renderCompleted(this.state.completedTasks)}
+        </ScrollView>
       </View>
-      )
+    )
   }
 })
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   header: {
     margin: 30,
@@ -116,26 +138,25 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18
   },
-  input: {
-    height: 60,
-    borderWidth:1,
-    borderColor: 'black',
-    borderRadius: 5,
-    textAlign: 'center',
-    margin: 10
-  },
   task: {
     flexDirection: 'row',
     height: 60,
     borderBottomWidth: 1,
     borderColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
     justifyContent: 'space-between',
-    padding: 20,
+    alignItems: 'center',
+    padding: 20
+  },
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    textAlign: 'center',
+    margin: 10
   },
   completed: {
-    color: '#777',
+    color: '#555',
     textDecorationLine: 'line-through'
   }
 })
